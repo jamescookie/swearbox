0f38c291ab2f0240a6a973ad6282db404edc0efd
James Cook <james.cookie@gmail.com>
2015-12-09 22:46:18 +0000

    Use common route for verbs and nouns, but make sure they don't repeat

--- public/js/swearbox.js
+++ public/js/swearbox.js
@@ -4,15 +4,24 @@ if (typeof exports !== "undefined") {
 
 (function (exports, _) {
     exports.swear = function () {
-        var verbs = ['licking', 'fucking', 'wanking', 'bashing', 'sucking', 'slapping', 'leaking', 'spitting', 'munching', 'gobbling'];
+        var verbs = ['fucking', 'wanking', 'leaking'];
         var common = ['lick', 'bash', 'suck', 'slapp', 'spitt', 'munch', 'gobbl', 'juggl'];
         var adjectives = ['shit', 'arse', 'scrotum', 'tit', 'fart', 'piss', 'cock', 'fuck', 'cunt', 'wank', 'bollock', 'spunk', 'twat', 'minge'];
-        var nouns = ['face', 'stain', 'nose', 'juggler', 'monkey', 'juice', 'candle', 'licker', 'sack', 'knob', 'worm', 'whale', 'jockey', 'flaps', 'bubble', 'whore'];
+        var nouns = ['face', 'stain', 'nose', 'monkey', 'juice', 'candle', 'sack', 'knob', 'worm', 'whale', 'jockey', 'bubble', 'whore'];
         var rand = function (it) {
             return it[Math.floor(Math.random() * it.length)]
         };
-        var adj = rand(adjectives);
-        adjectives.splice(_.contains(adjectives, adj), 1);
-        return adj + "-" + rand(verbs) + " " + rand(adjectives) + " " + rand(nouns);
+        var findFrom = function(list, common, suffix) {
+            var found = rand(_.union(list, common));
+            var fromCommon = _.contains(common, found);
+            return {
+                word: found + (fromCommon ? suffix : ''),
+                fromCommon: fromCommon
+            }
+        };
+        var adjective = rand(adjectives);
+        var verbContainer = findFrom(verbs, common, 'ing');
+        var noun = verbContainer.fromCommon ? rand(nouns) : findFrom(nouns, common, 'er').word;
+        return adjective + "-" + verbContainer.word + " " + rand(_.without(adjectives, adjective)) + " " + noun;
     };
 })(typeof exports === 'undefined' ? this['swearbox'] = {} : exports, _);
